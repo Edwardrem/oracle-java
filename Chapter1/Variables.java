4bea1104904faf60893424ad38757788961a9f14
Rufaro Edward Mutepfa <45569487+Edwardrem@users.noreply.github.com>
2024-05-05 11:29:43 +0200

    Managing Variable Scope

---
+++ Chapter1/Variables.java
@@ -0,0 +1,12 @@
+public class Variables {
+    public int valid(){
+        int y = 10;
+        int x;
+        x = 3;
+        int z;
+        int reply = x + y;
+        return reply;
+
+    }
+    
+}
