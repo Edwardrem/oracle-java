404d7069fe5e0895891427da3dcf547d74dd7e22
Rufaro Edward Mutepfa <45569487+Edwardrem@users.noreply.github.com>
2024-05-08 12:57:58 +0200

    Division and Modulus Operators

---
+++ Chapter2/Arithmetic.java
@@ -0,0 +1,18 @@
+package Chapter2;
+
+public class Arithmetic {
+    public static void main(String[] args) {
+        System.out.println(9/3);
+        System.out.println(9%3);
+        
+        System.out.println(10/3);
+        System.out.println(10%3);
+        
+        System.out.println(11/3);
+        System.out.println(11%3);
+
+        System.out.println(12/3);
+        System.out.println(12%3);
+    }
+    
+}
