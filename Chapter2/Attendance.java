932867c3618039006525afa5d7c81874b4ba9843
Rufaro Edward Mutepfa <45569487+Edwardrem@users.noreply.github.com>
2024-05-08 12:49:45 +0200

    Increment and Decrement Operators

---
+++ Chapter2/Attendance.java
@@ -0,0 +1,13 @@
+package Chapter2;
+
+public class Attendance {
+    public static void main(String[] args) {
+        int parkAttendance = 0;
+        System.out.println(parkAttendance);
+        System.out.println(++parkAttendance);
+        System.out.println(parkAttendance);
+        System.out.println(parkAttendance--);
+        System.out.println(parkAttendance);
+    }
+    
+}
