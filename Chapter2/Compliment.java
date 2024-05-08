6fea2dbc681378423bf357e3f29870f9196d2159
Rufaro Edward Mutepfa <45569487+Edwardrem@users.noreply.github.com>
2024-05-08 12:42:01 +0200

    compliment operators

---
+++ Chapter2/Compliment.java
@@ -0,0 +1,11 @@
+package Chapter2;
+
+public class Compliment {
+    public static void main(String[] args) {
+        boolean isAnimalAsleep = false;
+        System.out.println(isAnimalAsleep);
+        isAnimalAsleep = !isAnimalAsleep;
+        System.out.println(isAnimalAsleep);
+    }
+    
+}
