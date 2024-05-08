f3b659b0385019bcd2c88d943ec5b9b5cc47fff2
Rufaro Edward Mutepfa <45569487+Edwardrem@users.noreply.github.com>
2024-05-08 15:39:05 +0200

    The else Statement

---
+++ Chapter3/elseStatement.java
@@ -0,0 +1,16 @@
+package Chapter3;
+
+public class elseStatement {
+    public static void main(String[] args) {
+        int hourOfDay = 25;
+        if(hourOfDay < 11){
+            System.out.println("Good Morning");
+        } else if(hourOfDay < 15){
+            System.out.println("Good Afternoon ");
+        } else {
+            System.out.println("Good Evening");
+        } 
+    
+    }
+    
+}
