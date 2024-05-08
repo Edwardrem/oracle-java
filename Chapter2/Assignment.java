7696389b15be4b197602563f7375f0b2a4a6aed8
Rufaro Edward Mutepfa <45569487+Edwardrem@users.noreply.github.com>
2024-05-08 14:10:01 +0200

    Return Value of Assignment Operators

---
+++ Chapter2/Assignment.java
@@ -0,0 +1,15 @@
+package Chapter2;
+
+public class Assignment {
+    public static void main(String[] args) {
+        long wolf =5;
+        long coyote = (wolf=3);
+        System.out.println(wolf);
+        System.out.println(coyote);
+
+        boolean healthy = false;
+        if(healthy = true)
+            System.out.println("Good");
+    }
+    
+}
