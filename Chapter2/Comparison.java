d92eaab8aa119897be16dbd2d3046b9fbe52c79b
Rufaro Edward Mutepfa <45569487+Edwardrem@users.noreply.github.com>
2024-05-08 14:30:32 +0200

    Numeric Comparison Operators

---
+++ Chapter2/Comparison.java
@@ -0,0 +1,11 @@
+package Chapter2;
+
+public class Comparison {
+    public static void main(String[] args) {
+        int gibbonNumFeet = 2, wolfNumFeet = 4, ostrichNumFeet =2;
+        System.out.println(gibbonNumFeet < wolfNumFeet);
+        System.out.println(gibbonNumFeet <=wolfNumFeet);
+        System.out.println(gibbonNumFeet >= ostrichNumFeet);
+        System.out.println(gibbonNumFeet > ostrichNumFeet);
+    }
+}
